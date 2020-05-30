8b9c3db5c9b3f64e0399e67dc9dfd4531b2bb52c
Jeff Lefebvre <stamp@lanhub.ca>
2020-05-29 23:59:02 -0500

    adding console app

---
+++ SDRSharp.RTLSDR.UiConsole/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SDRSharp.Radio;
+
+namespace SDRSharp.RTLSDR.UiConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint deviceIndex = 0;
+           
+
+            var devices = DeviceDisplay.GetActiveDevices();
+            foreach (var deviceDisplay in devices)
+            {
+                deviceIndex = deviceDisplay.Index;
+                Console.WriteLine($"{deviceDisplay.Index}: {deviceDisplay.Name}");
+            }
+
+
+            using (var device = new RtlDevice(deviceIndex))
+            {
+                device.Start();
+                Console.ReadLine();
+
+                device.Stop();
+
+            }
+
+            var controller = new ExtIOController("C:\\TEMP\\extio.dll");
+
+            controller.Start();
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+            //using (var _rtlDevice = new RtlDevice(0)) 
+            //{
+            //    _rtlDevice.SamplesAvailable += rtlDevice_SamplesAvailable;
+            //    _rtlDevice.Frequency = _frequency;
+            //}
+        }
+
+       
+
+
+
+
+    }
+
+    
+}
+
+//namespace test
+//{
+//    public unsafe sealed class SignalsClass
+//    {
+
+//        private void FrontendFiller(IFrontendController sender, Complex* data, int len)
+//        {
+//            //if (_iqStream.Length < _inputBufferSize * 4)
+//            //{
+//            //    _iqStream.Write(samples, len);
+//            //}
+//        }
+//    }
+//}
